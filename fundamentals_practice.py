5171b79843b2c0e252094c5601317336c4819825
EricNguyen1213 <eric.nguyenblank@gmail.com>
2021-06-15 10:50:46 -0700

    cool

--- fundamentals_practice.py
+++ fundamentals_practice.py
@@ -1,2 +1,26 @@
 print("Hello world")
 #Hello world
+
+name = "LeBron"
+#String
+age= 35
+#Integer
+salary = 37.44
+#Float
+plays_basketball = True
+#Boolean
+jersey_number = "23"
+#String
+
+car_name = "Ford"
+x = 50
+To_my_first_name = "Eric"
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
+print(type(car_name))
+print(type(x))
+print(type(To_my_first_name))
